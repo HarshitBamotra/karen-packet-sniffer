ee9b7b13d82b279244dd1bb7861fed14cf92c7d9
Harshit Bamotra <harshitbamotra.01@gmail.com>
2025-07-19 14:13:07 +0530

    Parsing packets

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 
 	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
 	"github.com/google/gopacket/pcap"
 )
 
@@ -28,6 +29,28 @@ func main() {
 
 	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
 	for packet := range packetSource.Packets() {
-		fmt.Println(packet)
+		// fmt.Println(packet)
+		fmt.Println()
+
+		if ethLayer := packet.Layer(layers.LayerTypeEthernet); ethLayer != nil {
+			eth := ethLayer.(*layers.Ethernet)
+			fmt.Printf("Ethernet: %s -> %s | Type: %s\n", eth.SrcMAC, eth.DstMAC, eth.EthernetType)
+		}
+
+		if ipLayer := packet.Layer(layers.LayerTypeIPv4); ipLayer != nil {
+			ip := ipLayer.(*layers.IPv4)
+			fmt.Printf("IP: %s -> %s | Protocol: %s\n", ip.SrcIP, ip.DstIP, ip.Protocol)
+		}
+
+		if tcpLayer := packet.Layer(layers.LayerTypeTCP); tcpLayer != nil {
+			tcp := tcpLayer.(*layers.TCP)
+			fmt.Printf("TCP: %d → %d | SYN: %t, ACK: %t\n", tcp.SrcPort, tcp.DstPort, tcp.SYN, tcp.ACK)
+		}
+
+		if udpLayer := packet.Layer(layers.LayerTypeUDP); udpLayer != nil {
+			udp := udpLayer.(*layers.UDP)
+			fmt.Printf("UDP: %d → %d\n", udp.SrcPort, udp.DstPort)
+		}
+
 	}
 }
